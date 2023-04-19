09227fc20b191aa1190430290694702f34429629
Tyler Kirumba <Tyler>
2023-04-19 11:22:45 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -3,4 +3,34 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
+function ParentComponent(){
+    return
+    <ChildComponent text="Hello!" number ={2} />;
+}
+
+function ChildComponent(props){
+    return(
+        <div>
+            {props.name}{props.number}
+        </div>
+    )
+}
+
+//Parent Component
+function BlogPost(){
+    return(
+        <div>
+            <BlogContent articleText ="Dear Reader: Loren Ibsum"/>
+        </div>
+    );
+}
+
+function BlogContent(props){
+    return(
+        <div>
+            {props.articleText}
+        </div>
+    )
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
